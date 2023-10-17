7546df8b9cb3b1f9e99aac5b6e5902f471dd2232
ian <ian.luca202@gmail.com>
2023-10-17 17:38:15 -0300

    att

--- mail.php
+++ mail.php
@@ -2,11 +2,15 @@
 ini_set('display_errors', 1);
 require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
+
+$nome = $_POST['user_name'];
+$email = $_POST['user_email'];
+
 $mail = new PHPMailer;
 $mail->CharSet = "UTF-8";
 $mail->Encoding = 'base64';
 $mail->isSMTP();
-$mail->SMTPDebug = 0;
+$mail->SMTPDebug = 2;
 $mail->Host = 'smtp.hostinger.com';
 $mail->Port = 587;
 $mail->SMTPAuth = true;
@@ -14,7 +18,7 @@ $mail->Username = 'contato@palmilhasterapeuticas.com.br';
 $mail->Password = '@Podal2023';
 $mail->setFrom('contato@palmilhasterapeuticas.com.br', 'Contato Palmilhas Terapêuticas');
 $mail->addReplyTo('contato@palmilhasterapeuticas.com.br', 'Contato Palmilhas Terapêuticas');
-$mail->addAddress($_POST['user_email'], $_POST['user_name']);
+$mail->addAddress($email, $nome);
 $mail->Subject = '🚨 Falta muito pouco para garantir sua participação!';
 $mail->msgHTML(file_get_contents('mail/confirmacao.html'), __DIR__);
 if (!$mail->send()) {
