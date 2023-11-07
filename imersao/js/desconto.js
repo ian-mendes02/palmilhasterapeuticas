26b1b5267be47187250a03b3349d5a60b2550ac0
ian_mendes202 <ian.luca202@gmail.com>
2023-11-07 15:59:18 -0300

    deployment - 2023-11-07 15:59:18

--- imersao/js/desconto.js
+++ imersao/js/desconto.js
@@ -1,7 +1,7 @@
 const timer = document.getElementById("timer");
 document.getElementById("dismiss").addEventListener("click", () => timer.parentElement.remove())
 setInterval(()=> {
-    var final = new Date("November 8, 2023 00:00:00 GMT-03:00");
+    var final = new Date("November 8, 2023 12:00:00 GMT-03:00");
     var delta = (final.getTime() - Date.now());
     var d = Math.floor(delta / (1000 * 60 * 60 * 24));
     var h = Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
